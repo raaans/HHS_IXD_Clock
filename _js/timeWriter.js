b5d61572fb157af45289e98842367329025cb1ec
Rens <renshuizer@freedom.nl>
2020-03-21 16:18:18 +0100

    Add basic clock

---
+++ _js/timeWriter.js
@@ -0,0 +1,26 @@
+function writeTime() {
+    //get current time
+    var currentTime = new Date();
+
+    //create vars because writing takes effort
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+
+    //call addZero function for make nice
+    h = addZero(h);
+    m = addZero(m);
+
+    //write current time into span#currentGMT
+    document.getElementById('currentGMT').innerHTML = h + ":" + m;
+
+    //refreshes writeTime function every 5s
+    var t = setTimeout(writeTime, 5000);
+}
+
+//add a zero in front of single-digit numbers
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i
+    };
+    return i;
+}
